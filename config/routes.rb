da69a668a6a81e2eca3617c8fd9007c9e3b997a4
girishk <girishk5091@gmail.com>
2022-06-14 15:38:53 +0530

    create endpoints for payment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :loans, defaults: {format: :json}
+  resources :loans, defaults: { format: :json } do
+    member do
+      resources :payments, param: :payment_id, only: %i[index show create]
+    end
+  end
 end
